346695c22ab262097de67d558b7479c9466fef9f
TTobe <35289282+TTobe@users.noreply.github.com>
2023-03-18 14:13:22 +0900

    3/18

--- samsung/1244.py
+++ samsung/1244.py
@@ -8,18 +8,21 @@ for test_case in range(1, T + 1):
     switch = int(line[1])
     # 바꿔야 하는 횟수
     for i in range(switch):
-        bat = bonus[i:]
         # 이미 바꾼 앞자리를 제외 한 뒤
-        for k in range(len(bat)):
-            m = bonus.index(max(bat))
-            print("max bat", max(bat))
-            print("bonus index", m)
-
-            tmp = bonus[m]
-            
-            
-
-            
-
+        bat = bonus[i:]
+        # max index
+        maxindex = len(bonus)-1
+        # 이미 바꾼 앞자리 빼고까지 에서 max 검사해서 max index를 찾는데, 뒤에서 부터 찾기
+        for j in range(len(bat)):
+            if bonus[maxindex] < bonus[len(bonus)-1 - j]:
+                maxindex = len(bonus)-1 - j
+        print(maxindex, bonus[maxindex])
         
-        
+        tmp = bonus[i]
+        bonus[i] = bonus[maxindex]
+        bonus[maxindex] = tmp
+        print(i, bonus)
+
+    print("#{} {}".format(test_case, ''.join(map(str,bonus))))
+
+    #78466 2
