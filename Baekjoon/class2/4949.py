aa257f3752deab3185e223977438d6ca86a59006
TTobe <35289282+TTobe@users.noreply.github.com>
2023-04-27 09:07:23 +0900

    4/27

--- Baekjoon/class2/4949.py
+++ Baekjoon/class2/4949.py
@@ -11,20 +11,19 @@ while True:
     for i in range(len(arr)):
         if arr[i] == '[' or arr[i] == '(':
             stack.append(arr[i])
-        elif len(stack) != 0:
-            if arr[i] == ']':
-                if stack[-1] == '(':
-                    result = False
-                    break
-                elif stack[-1] == '[':
-                    stack.pop()
-            elif arr[i] == ')':
-                if stack[-1] == '[':
-                    result = False
-                    break
-                elif stack[-1] == '(':
-                    stack.pop()
-    # print(stack)
+        elif arr[i] == ']':
+            if len(stack) == 0 or stack[-1] == '(':
+                result = False
+                break
+            elif stack[-1] == '[':
+                stack.pop()
+        elif arr[i] == ')':
+            if len(stack) == 0 or stack[-1] == '[':
+                result = False
+                break
+            elif stack[-1] == '(':
+                stack.pop()
+# print(stack)
     if len(stack) != 0 or result == False:
         print('no') 
     else:
