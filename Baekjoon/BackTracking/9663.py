cc1e376afb6a874d9548898f56da49ea461f5849
jhhwang <101859033+ttobe@users.noreply.github.com>
2023-05-29 19:52:43 +0900

    BackTracking

--- Baekjoon/BackTracking/9663.py
+++ Baekjoon/BackTracking/9663.py
@@ -1,4 +1,39 @@
 # n-queen
 
-N = int(input())
+# N = int(input())
+N = 4
+arr = [[False for j in range(N)]for i in range(N)]
+global result
+result = 0
+
+def isOkay(x, i):
+    for k in range(x):
+        if i == k or abs(x - k) == abs(i - k):
+            return False
+        
+    return True
+
+def nqueen(x):
+    global result
+    if x == N:
+        result += 1
+        return
+    else:
+        for i in range(N):
+            # print(i)
+            print(x , i)
+            for l in range(N):
+                print(arr[l])
+
+            
+
+            if isOkay(x, i):
+                arr[x][i] = True
+                nqueen(x+1)
+                arr[x][i] = False
+
+nqueen(0)
+
+
+print(result)
 
--- Baekjoon/class2/1654.py
+++ Baekjoon/class2/1654.py
@@ -3,4 +3,25 @@
 K, N = map(int, input().split())
 
 # N개의 랜선을 만들려는데 K개의 무작위 선들이 있다.
+arr = []
+for i in range(K):
+    arr.append(int(input()))
 
+# print(arr)
+
+start = 1
+end = max(arr)
+
+while start <= end:
+    m = (start + end) //2
+    lines = 0
+    for i in arr:
+        lines += i // m
+    
+    if lines >= N:
+        start = m + 1
+    else:
+        end = m-1
+
+
+print (end)
