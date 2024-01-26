0a422cbe2ae276eff94cf5b9dc30237d1a9d69be
jhhwang <101859033+ttobe@users.noreply.github.com>
2024-01-27 03:06:38 +0900

    240126 자바 클래스2

---
+++ java/class2/Test1920.java
@@ -0,0 +1,34 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Test1920 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (Arrays.binarySearch(arr, x) < 0) {
+                System.out.println("0");
+            } else {
+                System.out.println("1");
+            }
+        }
+
+
+    }
+}
